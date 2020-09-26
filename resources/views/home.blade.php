c643a0c672f3ce0db25a203b561eb22b8c97d8c0
lucaslsg-prog <lsg.comunicacoes@gmail.com>
2020-09-26 13:54:14 -0400

    Dashboard adminlte

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,18 @@
-@extends('layouts.app')
+@extends('adminlte::page')
+
+@section('title','Home')
+
+@section('content_header')
+    <h1>Home</h1>
+@stop
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <h1>Welcome</h1>
+@stop
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+@section('css')
+@stop
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+@section('js')
+@stop
+
